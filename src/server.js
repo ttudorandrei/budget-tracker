61870ce4dcf512f015b4db26e201be37ab99f045
Tudor Tocan <tudor.tocan@gmail.com>
2021-07-25 12:27:28 +0100

    refactor

--- src/server.js
+++ src/server.js
@@ -17,12 +17,13 @@ app.use(express.static("public"));
 
 mongoose.connect("mongodb://localhost/budget", {
   useNewUrlParser: true,
-  useFindAndModify: false
+  useFindAndModify: false,
+  useUnifiedTopology: true
 });
 
 // routes
 app.use(require("./routes/api.js"));
 
 app.listen(PORT, () => {
-  console.log(`App running on port ${PORT}!`);
+  console.log(`App running on port http://localhost:${PORT}!`);
 });
